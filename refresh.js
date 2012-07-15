243051a22ae101e590a271febc3e7cfaea725d9d
waylybaye <havelove@gmail.com>
2012-07-15 19:12:01 +0800

    use {{ port }} variable in refresh.js

--- refresh.js
+++ refresh.js
@@ -1,3 +1,3 @@
-$.getJSON('http://localhost:1127/poll?callback=?', function(json){
+$.getJSON('http://localhost:{{ port }}/poll?callback=?', function(json){
   if( json.changed ){ window.location.reload(); }
 });
