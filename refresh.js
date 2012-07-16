55ff71764d08a518e929e3cb15434210281e1b51
waylybaye <havelove@gmail.com>
2012-07-16 16:11:11 +0800

    Use absolute path when read refresh.js

--- refresh.js
+++ refresh.js
@@ -1,3 +1,7 @@
-$.getJSON('http://localhost:{{ port }}/poll?callback=?', function(json){
-  if( json.changed ){ window.location.reload(); }
-});
+(function(window, undefined){
+  if(window.jQuery){
+    $.getJSON('http://localhost:{{ port }}/poll?callback=?', function(json){
+      if( json.changed ){ window.location.reload(); }
+    });
+  }
+})(window);
