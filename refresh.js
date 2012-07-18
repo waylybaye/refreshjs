631d71706e7a21d5ed47a1f92b0f5fcb6e9dbd17
waylybaye <havelove@gmail.com>
2012-07-18 14:29:41 +0800

    checking polling status in setInterval

--- refresh.js
+++ refresh.js
@@ -1,7 +1,18 @@
 (function(window, undefined){
-  if(window.jQuery){
-    $.getJSON('http://localhost:{{ port }}/poll?callback=?', function(json){
-      if( json.changed ){ window.location.reload(); }
-    });
+  var is_requesting = false;
+
+  function pollingChange(){
+    if(window.jQuery){
+      $.getJSON('http://localhost:{{ port }}/poll?callback=?', function(json){
+        if( json.changed ){ window.location.reload(); }
+        pollingChange();
+      });
+      is_requesting = true;
+    }
   }
+  setInterval(function(){
+    if( !is_requesting ){
+      pollingChange();
+    }
+  }, 100);
 })(window);
